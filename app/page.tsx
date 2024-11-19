e328d93cff3301a08773b9415ec42f255b6b891e
STRANGER <127176705+BADs9745@users.noreply.github.com>
2024-11-19 23:49:45 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
 	return (
 		<section>
 			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-				My Portfolio
+				My Portfolio Uwaw
 			</h1>
 			<div className="space-x-1 space-y-1">
 				<Link href={"/about"}>
